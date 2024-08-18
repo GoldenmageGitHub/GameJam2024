9a3778fab3551d0bbdc05b3dc62e1cfde9e2fa53
Fabian Pottbäcker <1104693+fpottbaecker@users.noreply.github.com>
2024-08-18 03:34:03 +0200

    Fix cat walk animation frames

--- pixelbypixel/scripts/scaling_camera_3d.gd
+++ pixelbypixel/scripts/scaling_camera_3d.gd
@@ -3,7 +3,7 @@ extends Camera3D
 @onready var practical_attributes: CameraAttributesPractical = $".".attributes
 @onready var initial_attributes: CameraAttributesPractical = practical_attributes.duplicate()
 
-func _process(delta: float) -> void:
+func _process(_delta: float) -> void:
 	var scale_value = get_parent_node_3d().scale.x
 	practical_attributes.dof_blur_far_distance = initial_attributes.dof_blur_far_distance * scale_value
 	practical_attributes.dof_blur_far_transition = initial_attributes.dof_blur_far_transition * scale_value
